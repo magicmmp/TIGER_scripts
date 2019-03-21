071db095e228fd6e9b3bdb2a987febc98e0dd706
magicmmp <2233973404@qq.com>
2019-03-22 05:15:20 +0800

    git push with shell: Fri Mar 22 05:15:20 CST 2019

--- TM_DATA_consumer.py
+++ TM_DATA_consumer.py
@@ -14,7 +14,7 @@ HOST_PORT = 58880
 
 #Total_data_size = 2**24
 #Total_data_size = 2**16
-Total_data_size = 2**10
+Total_data_size = 1024
 BUFSIZE = 4096
 FEB_index = 0
 data_file = 'FEB%d_data.txt'% FEB_index
